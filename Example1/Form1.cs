3ce2ca865deb8a4ec24e0e535ac294819f1845ce
Storqh <tranquochuyk18@siu.edu.vn>
2026-09-15 21:37:09 +0700

    cap nhat nhanh con

--- Example1/Form1.cs
+++ Example1/Form1.cs
@@ -3,6 +3,11 @@ namespace Example1
     public partial class Form1 : Form
     {
         //cap nhat lan 1
+        //cap nhat nhanh con
+        //cap nhat nhanh con lan 1
+        //cap nhat nhanh con lan 2
+        //cap nhat nhanh con lan 3
+        //cap nhat nhanh con lan 4
         public Form1()
         {
             InitializeComponent();
